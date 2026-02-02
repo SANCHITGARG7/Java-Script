e2386006ea43379148b12314f0cf53eacf542112
Sanchit Garg <sanchitgarg0111@gmail.com>
2026-02-03 05:04:14 +0530

    Add files via upload

---
+++ conditional.js
@@ -0,0 +1,30 @@
+// conditional statements
+let age = 30; 
+
+//if statements
+
+if (age > 18) {
+   console.log("You are eligible to vote.");
+}
+
+//if-else statements
+
+if (age >= 20) {
+   console.log("You are eligible for drinking");
+}
+else{
+   console.log("Not eligible for drinking");
+}
+
+
+// Else-if Statements
+
+if (age<18){
+   console.log("Person is a Minor")
+}
+else if (age <= 30 && age >=18){
+   console.log("Person is an Adult")
+}
+else {
+   console.log("Person is Old Age")
+}
