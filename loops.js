ca1c935b24d7fec5aad62a76e61cf406a010ed61
Sanchit Garg <sanchitgarg0111@gmail.com>
2026-02-04 03:05:36 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,27 @@
+
+//For loop
+console.log("For Loop:");
+
+for(let i=1;i<=5;i++){
+    console.log("SnchitGarg")
+}
+
+
+//While Loop
+console.log("While Loop:");
+
+let numb = 1;
+while(numb < 10){
+    console.log("numb",numb);
+    numb++;
+}
+
+
+// do while
+console.log("Do While Loop:");
+
+let i=0;
+do{
+    console.log("number",i);
+    i++;
+}while(i<=7);
