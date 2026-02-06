cc5b81be9ada257953e0905d78a19d4e34ee04c0
Sanchit Garg <sanchitgarg0111@gmail.com>
2026-02-06 09:12:36 +0530

    Add files via upload

--- loops.js
+++ loops.js
@@ -17,7 +17,7 @@ while(numb < 10){
 }
 
 
-// do while
+// do while Loop
 console.log("Do While Loop:");
 
 let i=0;
@@ -25,3 +25,27 @@ do{
     console.log("number",i);
     i++;
 }while(i<=7);
+
+
+// for of Loop
+
+let name = "Sanchit Garg"
+let size = 0
+for(let i of name){
+    console.log("i= ",i);
+    size++;
+}
+
+console.log("Size =", size);
+
+// For In Loop
+
+let student = {
+    name: "sanchit",
+    ge : 20,
+    CGP : 7.88,
+    IsPass : true
+}
+for (let key in student){
+    console.log("key :", key + ":", student[key]);
+}
