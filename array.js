7725a64ee11c64e887ba68168c97023876eeea33
Sanchit Garg <sanchitgarg0111@gmail.com>
2026-03-01 01:27:55 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,18 @@
+// Collection of Similar data
+
+// Creating array
+
+let Hero = ["ironman","spiderman","batman"];
+let marks = [92,93,97,95];
+
+console.log(Hero);
+console.log(Hero.length);
+
+console.log(marks);
+console.log(marks.length); // property of array
+
+// Array indices
+
+console.log(Hero[0]);
+console.log(Hero[1]);
+console.log(Hero[2]);
