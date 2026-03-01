9913a16804386818ad473dbe92dfb6449a5cff97
Sanchit Garg <sanchitgarg0111@gmail.com>
2026-03-02 02:27:52 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,26 @@
+// function is a block of code to perform a specific task
+
+// fuction creation
+
+function print(){
+    console.log("Meowwwwwwwwwwwww");
+    console.log("Na Maane");
+}
+
+print();
+
+
+function msg(msg){  // parameter -> input
+    console.log(msg);
+    console.log("Hello World");
+}
+
+msg(90); // argument
+
+
+// function  ->  sum of 2 number
+
+function sum(x,y){
+    console.log(x+y)
+}
+sum(100,4);
