d50766f778ef7eb08047ddffcc23fe4ff47d3d7a
Sanchit Garg <sanchitgarg0111@gmail.com>
2026-03-02 02:28:08 +0530

    Add files via upload

---
+++ loopinarray.js
@@ -0,0 +1,17 @@
+// Loops in array
+
+let Hero = ["ironman","spiderman","batman","superman","thor"];
+
+// For Loop in Array
+
+for (let i = 0; i<Hero.length; i++ ){
+    console.log(Hero[i])
+}
+
+
+//for off loop for array
+
+for(let h of Hero){
+    console.log(h);
+}
+
