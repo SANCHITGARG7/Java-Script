464d39cc6ed2ab4dedf9428364b7166c8a9c868c
Sanchit Garg <sanchitgarg0111@gmail.com>
2026-03-02 02:27:15 +0530

    Add files via upload

---
+++ arraymethod.js
@@ -0,0 +1,28 @@
+// Methods used in Array. Not all but basics
+
+let info = ["sanchit",20,"btech","man"]
+let marvel = ["ironmn","spidermn"]
+let dc = ["batman","superman"]
+
+//push (add at end)
+
+info.push("Third Year","badmosh");
+console.log(info);
+
+
+//pop (delete from end)
+
+info.pop();
+console.log(info);
+
+
+// to string (converts full array in string)
+
+let info1 = info.toString();
+console.log(info1);
+
+
+//concat of array 
+
+let heros = marvel.concat(dc);
+console.log(heros);
