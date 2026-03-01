5851b863aeffca27a2d7fa84f5c831fd703a246d
Sanchit Garg <sanchitgarg0111@gmail.com>
2026-03-02 02:27:31 +0530

    Add files via upload

---
+++ arrowfunction.js
@@ -0,0 +1,18 @@
+// compact way of writing functions 
+
+
+// function creation 
+
+// 1. sum function 
+
+const sum = (a,b)=>{
+   console.log(a+b);
+}
+sum(10,5);
+
+// 2. Mul. function
+
+const mul=(x,y)=>{
+console.log(x*y);
+}
+mul(10,2);
